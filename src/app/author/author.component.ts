dfbad4ba47b65ce6645eced031fd0da98fb51e68
wishwaprabodha <ww46673@gmail.com>
2019-01-16 00:57:20 +0530

    POST implemented

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -8,16 +8,29 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./author.component.css']
 })
 export class AuthorComponent {
-
-
   au: any[] = [];
 
+
   constructor(private http: HttpClient) {
-    http.get<any[]>('http://localhost:3000/api/authors')
+    this.viewAuthors();
+  }
+
+  viewAuthors() {
+    this.http.get<any[]>('http://localhost:3000/api/authors')
       .subscribe((res: any[]) => {
         console.log(res);
         this.au = res;
       });
   }
+
+  addAuthor(fg: any) {
+    this.http.post('http://localhost:3000/api/authors', fg)
+      .subscribe(res => {
+        console.log(res);
+      });
+
+
+    }
+
 
 }
